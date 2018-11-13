cbec7cce6abf2741e5992c52f4e6bea59896c8b0
George Lucas T. Bentes <george.bentes@gmail.com>
2018-11-12 22:48:45 -0400

    handling books without thumbanils

--- src/Book.js
+++ src/Book.js
@@ -17,7 +17,13 @@ class Book extends Component{
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")`  }}></div>
+          <div className="book-cover" 
+                style={{ width: 128, 
+                         height: 193, 
+                         backgroundImage: `url( ${book.imageLinks && book.imageLinks.thumbnail ? 
+                                               `${book.imageLinks.thumbnail}` : 
+                                               `http://via.placeholder.com/128x193?text=No%20Cover`})` }}>
+          </div>
           <div className="book-shelf-changer">
             <select onChange={(event) => this.changeShelf(event)} value={book.shelf}>
               <option value="move" disabled>Move to...</option>
